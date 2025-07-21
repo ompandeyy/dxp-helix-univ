e00700b402d4eb061781c9f740cbbed9d6d0e03e
ompandeyy <ompandeyinfy@gmail.com>
2025-07-21 15:58:38 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -125,6 +125,7 @@ async function loadLazy(doc) {
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  import('../tools/sidekick/aem-genai-variations.js');
   loadFonts();
 }
 
